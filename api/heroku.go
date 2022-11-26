65d092220efed7569627cfc10e259e0847fad044
Gabriel Bento <67703889+gotneb@users.noreply.github.com>
2022-11-26 11:31:13 -0400

    Added  endpoint to listen recentMangas
    This new endpoint will allow to me send mangas from sites which uses AJAX.
    
    The reason is cuz go.Colly cannot scrape from sites which uses AJAX. Hence, I'll use others tools to make this.

--- api/heroku.go
+++ api/heroku.go
@@ -9,8 +9,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gotneb/manga_api/db"
 	"github.com/gotneb/manga_api/server"
+	"github.com/gotneb/manga_api/utils"
 	"github.com/gotneb/manga_api/web"
 )
+
+type Releases struct {
+	Links []string `json:"links" validate:"dive"`
+}
 
 func Init() {
 	r := gin.Default()
@@ -76,23 +81,46 @@ func Init() {
 		}
 	})
 
-	r.POST("/add/manga/:password", func(c *gin.Context) {
-		pass := c.Param("password")
-		if pass != db.PasswordUpload {
-			c.String(http.StatusBadRequest, "Password key to upload is wrong.")
+	/*
+		r.POST("/add/manga/:password", func(c *gin.Context) {
+			pass := c.Param("password")
+			if pass != db.PasswordUpload {
+				c.String(http.StatusBadRequest, "Password key to upload is wrong.")
+				return
+			}
+
+			var manga web.Manga
+			err := c.BindJSON(&manga)
+			if err != nil {
+				c.String(http.StatusBadGateway, err.Error())
+			}
+			db.AddManga(2, &manga)
+			c.String(http.StatusOK, "Saved with sucess")
+		})
+	*/
+
+	/*
+	 * TLDR; Don't worry about this, it's an endpoint where only I can use :).
+	 *
+	 * The majority of sites they use AJAX to load content, with this aproach, I'm not able to
+	 * scrape data from them. In this case I can use another tool (Seleniun) to get data and send it for here.
+	 *
+	 * [WARNING]: You might expect try this endpoint... But "utils" need authenthication, so if you try, then you fail.
+	 */
+	r.POST("/add/release-mangas/", func(c *gin.Context) {
+		var data Releases
+		err := c.BindJSON(&data)
+
+		if err != nil {
+			c.String(http.StatusBadGateway, err.Error())
 			return
 		}
 
-		var manga web.Manga
-		err := c.BindJSON(&manga)
-		if err != nil {
-			c.String(http.StatusBadGateway, err.Error())
-		}
-		db.AddManga(2, &manga)
+		utils.UploadRecentMangasFrom(db.MEUS_MANGAS, data.Links)
 		c.String(http.StatusOK, "Saved with sucess")
 	})
 
-	// Heroku
+	// Listen to port
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
--- utils/recent_mangas.go
+++ utils/recent_mangas.go
@@ -2,11 +2,9 @@ package utils
 
 import (
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/gocolly/colly"
 	"github.com/gotneb/manga_api/db"
 	serv "github.com/gotneb/manga_api/server"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -16,10 +14,10 @@ import (
 UploadRecentMangasFrom goes to specified server and get all recent mangas updated
 and stores it into the database.
 */
-func UploadRecentMangasFrom(server int) {
+func UploadRecentMangasFrom(server int, links []string) {
 	switch server {
 	case db.MEUS_MANGAS:
-		getFromMeusMangas()
+		getFromMeusMangas(links)
 	case db.MANGAINN:
 		getFromMangainn()
 	default:
@@ -27,43 +25,20 @@ func UploadRecentMangasFrom(server int) {
 	}
 }
 
-// Get all recent mangas from "meusmangas.net"
-func getRecentUpdates(page int) (links []string) {
-	c := colly.NewCollector()
-	link := fmt.Sprintf("https://meusmangas.net/comienzo/page/%d", page)
-
-	c.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting:", r.URL)
-	})
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
-	})
-	c.OnHTML("div#recent_releases  li.item_news-manga a.pull-left", func(e *colly.HTMLElement) {
-		link := "https://meusmangas.net" + e.Attr("href")
-		links = append(links, link)
-		log.Println(link)
-	})
-
-	c.Visit(link)
-
-	return
-}
-
-func getFromMeusMangas() {
+func getFromMeusMangas(recentLinks []string) {
 	stop := false
-	i := 1
 
 	client := serv.GetClient(db.MEUS_MANGAS)
 	serverCode := db.MEUS_MANGAS
 
 	for !stop {
-		links := getRecentUpdates(i)
+		links := recentLinks
 		for _, link := range links {
 			manga, stt := client.GetMangaDetail(link)
 			if stt != http.StatusOK {
 				log.Fatalln("manga not found")
 			}
-			// If do not exists, add it
+			// If do not exist, add it
 			_, err := db.FindManga(serverCode, manga.Title)
 			if err == mongo.ErrNoDocuments {
 				db.AddManga(serverCode, &manga)
@@ -88,3 +63,30 @@ func getFromMeusMangas() {
 func getFromMangainn() {
 
 }
+
+/*
+ * Get all recent mangas from "mymangas.net"
+ * WARNING: Because the "mymangas.net" currently it's loading recent from AJAX
+ * go.Colly is no longer avaliable to do this task
+ * So... It'll be REPLACED!
+func getRecentUpdates(page int) (links []string) {
+	c := colly.NewCollector()
+	link := fmt.Sprintf("https://meusmangas.net/comienzo/page/%d", page)
+
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("Visiting:", r.URL)
+	})
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+	c.OnHTML("div#recent_releases  li.item_news-manga a.pull-left", func(e *colly.HTMLElement) {
+		link := "https://meusmangas.net" + e.Attr("href")
+		links = append(links, link)
+		log.Println(link)
+	})
+
+	c.Visit(link)
+
+	return
+}
+*/
